268f49471d978854cdf5cca534bf2cc194554276
hano7758 <228330583@qq.com>
2019-07-12 14:20:20 +0800

    Add files via upload

---
+++ WaterMark/WaterMark/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+
+namespace WaterMark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WaterMark.AddWaterMark(@"D:\20.jpg", @"D:\WaterMark");
+        }
+
+        /// <summary>
+        /// 数字转中文
+        /// </summary>
+        /// <param name="number">eg: 22</param>
+        /// <returns></returns>
+        public static string NumberToChinese(int number)
+        {
+            var str = number.ToString();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                //int geWei = number % 10;
+                //int shiWei = number / 10 % 10;
+                //int baiWei = number / 100 % 10;
+                //int qianWei = number / 1000;
+
+
+            }
+
+            string res = string.Empty;
+            string schar = "";
+            switch (schar)
+            {
+                case "1":
+                    res = "一";
+                    break;
+                case "2":
+                    res = "二";
+                    break;
+                case "3":
+                    res = "三";
+                    break;
+                case "4":
+                    res = "四";
+                    break;
+                case "5":
+                    res = "五";
+                    break;
+                case "6":
+                    res = "六";
+                    break;
+                case "7":
+                    res = "七";
+                    break;
+                case "8":
+                    res = "八";
+                    break;
+                case "9":
+                    res = "九";
+                    break;
+                default:
+                    res = "零";
+                    break;
+            }
+
+            return res;
+        }
+        
+        public static string CmycurD(decimal num)
+        {
+            string str1 = "零壹贰叁肆伍陆柒捌玖";            //0-9所对应的汉字 
+            string str2 = "万仟佰拾亿仟佰拾万仟佰拾元角分"; //数字位所对应的汉字 
+            string str3 = "";    //从原num值中取出的值 
+            string str4 = "";    //数字的字符串形式 
+            string str5 = "";  //人民币大写金额形式 
+            int i;    //循环变量 
+            int j;    //num的值乘以100的字符串长度 
+            string ch1 = "";    //数字的汉语读法 
+            string ch2 = "";    //数字位的汉字读法 
+            int nzero = 0;  //用来计算连续的零值是几个 
+            int temp;            //从原num值中取出的值 
+
+            num = Math.Round(Math.Abs(num), 2);    //将num取绝对值并四舍五入取2位小数 
+            str4 = ((long)(num * 100)).ToString();        //将num乘100并转换成字符串形式 
+            j = str4.Length;      //找出最高位 
+            if (j > 15) { return "溢出"; }
+            str2 = str2.Substring(15 - j);   //取出对应位数的str2的值。如：200.55,j为5所以str2=佰拾元角分 
+
+            //循环取出每一位需要转换的值 
+            for (i = 0; i < j; i++)
+            {
+                str3 = str4.Substring(i, 1);          //取出需转换的某一位的值 
+                temp = Convert.ToInt32(str3);      //转换为数字 
+                if (i != (j - 3) && i != (j - 7) && i != (j - 11) && i != (j - 15))
+                {
+                    //当所取位数不为元、万、亿、万亿上的数字时 
+                    if (str3 == "0")
+                    {
+                        ch1 = "";
+                        ch2 = "";
+                        nzero = nzero + 1;
+                    }
+                    else
+                    {
+                        if (str3 != "0" && nzero != 0)
+                        {
+                            ch1 = "零" + str1.Substring(temp * 1, 1);
+                            ch2 = str2.Substring(i, 1);
+                            nzero = 0;
+                        }
+                        else
+                        {
+                            ch1 = str1.Substring(temp * 1, 1);
+                            ch2 = str2.Substring(i, 1);
+                            nzero = 0;
+                        }
+                    }
+                }
+                else
+                {
+                    //该位是万亿，亿，万，元位等关键位 
+                    if (str3 != "0" && nzero != 0)
+                    {
+                        ch1 = "零" + str1.Substring(temp * 1, 1);
+                        ch2 = str2.Substring(i, 1);
+                        nzero = 0;
+                    }
+                    else
+                    {
+                        if (str3 != "0" && nzero == 0)
+                        {
+                            ch1 = str1.Substring(temp * 1, 1);
+                            ch2 = str2.Substring(i, 1);
+                            nzero = 0;
+                        }
+                        else
+                        {
+                            if (str3 == "0" && nzero >= 3)
+                            {
+                                ch1 = "";
+                                ch2 = "";
+                                nzero = nzero + 1;
+                            }
+                            else
+                            {
+                                if (j >= 11)
+                                {
+                                    ch1 = "";
+                                    nzero = nzero + 1;
+                                }
+                                else
+                                {
+                                    ch1 = "";
+                                    ch2 = str2.Substring(i, 1);
+                                    nzero = nzero + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (i == (j - 11) || i == (j - 3))
+                {
+                    //如果该位是亿位或元位，则必须写上 
+                    ch2 = str2.Substring(i, 1);
+                }
+                str5 = str5 + ch1 + ch2;
+
+                if (i == j - 1 && str3 == "0")
+                {
+                    //最后一位（分）为0时，加上“整” 
+                    str5 = str5 + '整';
+                }
+            }
+            if (num == 0)
+            {
+                str5 = "零元整";
+            }
+            return str5;
+        }
+        
+    }
+}
---
+++ WaterMark/WaterMark/WaterMark.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace WaterMark
+{
+    class WaterMark
+    {
+        public static string AddWaterMark(string imgPath, string savePath)
+        {
+            var text = "仅供“XXXXXXXXXXXXXXXXXXXXX项目”使用";
+            var imageName = Path.GetFileNameWithoutExtension(imgPath);
+            var sImgPath = string.Format(@"{0}\{1}_Mark.jpg", savePath, imageName);
+            using (Image image = Image.FromFile(imgPath))
+            {
+                try
+                {
+                    Bitmap bitmap = new Bitmap(image);
+                    //图片的宽度与高度
+                    int width = bitmap.Width, height = bitmap.Height;
+                    //水印文字
+                    Graphics g = Graphics.FromImage(bitmap);
+                    g.DrawImage(bitmap, 0, 0);
+                    g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+                    g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                    g.DrawImage(image, new Rectangle(0, 0, width, height), 0, 0, width, height, GraphicsUnit.Pixel);
+                    Font crFont = new Font("微软雅黑", 22, FontStyle.Bold);
+                    SolidBrush semiTransBrush = new SolidBrush(Color.FromArgb(255, 137, 131, 131));
+                    //将原点移动 到图片中点
+                    g.TranslateTransform(-80, 150);
+                    //以原点为中心 转 -45度
+                    g.RotateTransform(-45);
+                    for (int i = 0; i < 50; i++)
+                    {
+                        g.DrawString(text, crFont, semiTransBrush, new PointF(0 - i * 300, 100 + i * 300));
+                        g.DrawString(text, crFont, semiTransBrush, new PointF(150 - i * 300, 500 + i * 300));
+                        g.DrawString(text, crFont, semiTransBrush, new PointF(650 - i * 300, 600 + i * 300));
+                    }
+                    //保存文件
+                    bitmap.Save(sImgPath, ImageFormat.Jpeg);
+                    return sImgPath;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+
+    }
+}
